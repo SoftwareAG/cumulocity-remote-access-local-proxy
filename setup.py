515da0626a59cc2e80e7a084839fc4c940cd5003
Stefan Witschel <stefan.witschel@softwareag.com>
2021-06-10 06:45:59 +0000

    Version 1.3.2

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@
 from setuptools import setup
 
 setup(name='c8ylp',
-      version='1.3.1',
+      version='1.3.2',
       description='Cumulocity Local Client Proxy',
       author='Stefan Witschel',
       license='Apache v2',

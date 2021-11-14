d07d6b5413e151c96e8e70a673775f38f1b9595d
Reuben Miller <reuben.miller@softwareag.com>
2021-11-14 20:39:22 +0100

    add dev dependencies

--- tasks.py
+++ tasks.py
@@ -1,4 +1,5 @@
 """Tasks"""
+import os
 from invoke import task
 
 
@@ -25,9 +26,58 @@ def build(c):
 def lint(c):
     """Lint"""
     c.run("python3 -m pylint c8ylp")
+    c.run("python3 -m pylint tests/")
 
 
 @task
 def format(c):
     """Format code (using black)"""
     c.run("python3 -m black --target-version=py37 .")
+
+
+@task
+def test(c, pattern=None):
+    """Run unit tests and coverage report"""
+    cmd = [
+        "python3",
+        "-m",
+        "pytest",
+        "--timeout=10",
+        # Note: Dont use log cli level (--log-cli-level) as it can affect click testing!
+        "--cov-config=.coveragerc",
+        "--cov-report=term",
+        "--cov-report=html:test_output/htmlcov",
+        "--cov=c8ylp",
+    ]
+
+    if pattern:
+        cmd.append(f"-k={pattern}")
+    c.run(" ".join(cmd))
+
+
+@task
+def test_performance(c, testid=2):
+    """
+    Run performance tests for specific scenarios
+    """
+
+    pypaths = [
+        os.getcwd(),
+        os.getenv("PYTHONPATH"),
+    ]
+    pypaths = [x for x in pypaths if x]
+
+    c.run("python3 ./tests/performance/create-file.py ./tests/performance/5.mb 5")
+
+    env = {
+        **os.environ,
+        "C8Y_DEVICE": "cb4-a2euccg1p22111000141",
+        "PYTHONPATH": ":".join(pypaths),
+    }
+
+    if testid == 1:
+        c.run("./tests/performance/test1_file_transfer.sh", env=env)
+    elif testid == 2:
+        c.run("./tests/performance/test2_file_transfer_concurrent.sh", env=env)
+    elif testid == 3:
+        c.run("./tests/performance/test3_concurrent_single_commands.sh", env=env)

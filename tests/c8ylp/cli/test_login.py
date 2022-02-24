5129c67a9bb9ff7834a78ab8e6b0acf56c9ee0b4
Reuben Miller <reuben.d.miller@gmail.com>
2022-02-24 15:13:25 +0100

    Issue 45/duplicate t option (#46)
    * fix: remove duplicate shorthand option
    * fix: add new option to disable storage of token in an dotenv file using --no-store-token

--- tests/c8ylp/cli/test_login.py
+++ tests/c8ylp/cli/test_login.py
@@ -197,3 +197,156 @@ def test_help_without_host(env: Environment):
     assert result.exit_code == 0
     assert "Host: " not in result.output, "User should not be prompted for hostname"
     assert "dummyinput" not in result.output, "User should not be prompted for hostname"
+
+
+@pytest.mark.parametrize(
+    "inputs",
+    (
+        {
+            "stdin": [],
+            "args": [
+                "--host",
+                "https://example.c8y.io",
+                "-u",
+                "myuser@company.com",
+                "-p",
+                "my-dummy45-password",
+                "-t",
+                "t12345",
+                "--token",
+                "98765",
+            ],
+            "env": {},
+        },
+    ),
+)
+def test_shorthand_login_options(
+    inputs, c8yserver: FixtureCumulocityAPI, env: Environment, tmpdir
+):
+    """Test shorthand options when logging in"""
+
+    @responses.activate
+    def run():
+        c8yserver.simulate_loginoptions()
+        c8yserver.simulate_login_oauth(status_codes=[401, 200])
+        c8yserver.simulate_current_user()
+
+        env_file = tmpdir.join(".env")
+        stdin = "".join(inputs["stdin"])
+
+        args = ["login", "--env-file", env_file.strpath, *inputs["args"]]
+
+        runner = CliRunner()
+        result = runner.invoke(
+            cli,
+            args,
+            env={
+                **env.create_empty_env(),
+                **inputs["env"],
+            },
+            input=stdin,
+        )
+
+        assert result.exit_code == 0
+
+        settings = env.read_file(env_file)
+        assert settings == {
+            "C8Y_HOST": "https://example.c8y.io",
+            "C8Y_USER": "myuser@company.com",
+            "C8Y_TENANT": "t12345",
+            "C8Y_TOKEN": "98765",
+        }
+
+    run()
+
+
+@pytest.mark.parametrize(
+    "inputs",
+    (
+        {
+            # Default behaviour is to store the token to the given --env-file
+            "stdin": [],
+            "args": ["--token", "98765"],
+            "env": {
+                "C8Y_HOST": "https://example.c8y.io",
+                "C8Y_USER": "myuser@company.com",
+                "C8Y_TENANT": "t12345",
+            },
+            "expected_env": {
+                "C8Y_HOST": "https://example.c8y.io",
+                "C8Y_USER": "myuser@company.com",
+                "C8Y_TENANT": "t12345",
+                "C8Y_TOKEN": "98765",
+            },
+        },
+        # User explicitly wants to store the token
+        {
+            "stdin": [],
+            "args": ["--token", "98765", "--store-token"],
+            "env": {
+                "C8Y_HOST": "https://example.c8y.io",
+                "C8Y_USER": "myuser@company.com",
+                "C8Y_TENANT": "t12345",
+            },
+            "expected_env": {
+                "C8Y_HOST": "https://example.c8y.io",
+                "C8Y_USER": "myuser@company.com",
+                "C8Y_TENANT": "t12345",
+                "C8Y_TOKEN": "98765",
+            },
+        },
+        # User explicitly does not want to store the token
+        {
+            "stdin": [],
+            "args": ["--token", "98765", "--no-store-token"],
+            "env": {
+                "C8Y_HOST": "https://example.c8y.io",
+                "C8Y_USER": "myuser@company.com",
+                "C8Y_TENANT": "t12345",
+            },
+            "expected_env": {
+                "C8Y_HOST": "https://example.c8y.io",
+                "C8Y_USER": "myuser@company.com",
+                "C8Y_TENANT": "t12345",
+            },
+        },
+    ),
+)
+def test_disable_token_storage(
+    inputs, c8yserver: FixtureCumulocityAPI, env: Environment, tmpdir
+):
+    """Test disabling of the token storage by excluding it from the environment file"""
+
+    @responses.activate
+    def run():
+        c8yserver.simulate_loginoptions()
+        c8yserver.simulate_login_oauth(status_codes=[401, 200])
+        c8yserver.simulate_current_user()
+
+        env_file = tmpdir.join(".env")
+
+        # Seed the env file with some default values
+        env_file.write_text(
+            "\n".join([f"{key}={value}" for key, value in inputs["env"].items()]),
+            "utf8",
+        )
+
+        stdin = "".join(inputs["stdin"])
+
+        runner = CliRunner()
+        result = runner.invoke(
+            cli,
+            ["login", "--env-file", env_file.strpath, *inputs["args"]],
+            env={
+                **env.create_empty_env(),
+                **inputs["env"],
+            },
+            input=stdin,
+        )
+
+        assert result.exit_code == 0
+
+        settings = env.read_file(env_file)
+        assert settings == inputs["expected_env"]
+
+    run()
